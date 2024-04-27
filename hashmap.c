999549dfd4e15eec3bbd6101550f74c46793c445
gogo <132955136+Degoriv@users.noreply.github.com>
2024-04-27 01:11:26 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -52,8 +52,11 @@ void enlarge(HashMap * map) {
 
 
 HashMap * createMap(long capacity) {
-
-    return NULL;
+	HashMap *map = (HashMap *)malloc(sizeof(HashMap));
+	if (map == NULL) {
+			return NULL; }
+	map->current = -1;
+	return map;
 }
 
 void eraseMap(HashMap * map,  char * key) {    
