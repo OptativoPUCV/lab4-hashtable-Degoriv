afc8334f75835637a6492a845ffb4c21c0178826
gogo <132955136+Degoriv@users.noreply.github.com>
2024-04-28 21:07:36 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -76,7 +76,6 @@ HashMap * createMap(long capacity) {
 void eraseMap(HashMap * map,  char * key) {    
 	long indice = hash(key, map->capacity);
 	map->buckets[indice]->key = NULL;
-	map->buckets[indice]->value = NULL;
 	map->size -= 1;
 }
 
