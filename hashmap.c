678707294bdb8238df3a853dc5c0e36da12da46d
gogo <132955136+Degoriv@users.noreply.github.com>
2024-04-29 01:32:25 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -60,7 +60,7 @@ void enlarge(HashMap * map) {
 
 	Pair** aux = map->buckets;
 	map->capacity *= 2;
-	map->buckets = (Pair**) malloc(sizeof(Pair*) * (map->capacity * 2));
+	map->buckets = (Pair**) malloc(sizeof(Pair*) * (map->capacity));
 	map->size = 0;
 
 	for (int i = 0 ; i < map->capacity ; i++)
